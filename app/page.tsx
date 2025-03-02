af24b43ec49c7edddad08b38e271aec1b0a77409
sajid1108 <cahsekolahbelajar@gmail.com>
2025-03-02 14:33:01 +0700

    lanyard

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,14 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <h1>Hallo Dunia</h1>;
+  return (
+    <div className="min-h-screen overflow-x-hidden">
+      <div className="container border mx-auto h-screen">
+        <div className="grid grid-cols-12">
+          <div className="col-span-6">A</div>
+          <div className="col-span-6">B</div>
+        </div>
+      </div>
+    </div>
+  );
 }
