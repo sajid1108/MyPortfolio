cdfda62bb7e3e5b4ed4d117d62c58a8878ec47f9
sajid1108 <cahsekolahbelajar@gmail.com>
2025-03-08 23:00:12 +0700

    change rotating text position

--- app/page.tsx
+++ app/page.tsx
@@ -7,19 +7,20 @@ export default function Home() {
     <div className="min-h-screen overflow-x-hidden bg-[#19222D]">
       <div className="container border mx-auto h-screen">
         <div className="grid grid-cols-12">
-          <div className="col-span-6">A</div>
-          <RotatingText
-            texts={["Student", "Junior Front End"]}
-            mainClassName="px-2 sm:px-2 md:px-3 bg-cyan-300 text-black overflow-hidden py-0.5 sm:py-1 md:py-2 justify-center rounded-lg"
-            staggerFrom={"last"}
-            initial={{ y: "100%" }}
-            animate={{ y: 0 }}
-            exit={{ y: "-120%" }}
-            staggerDuration={0.025}
-            splitLevelClassName="overflow-hidden pb-0.5 sm:pb-1 md:pb-1"
-            transition={{ type: "spring", damping: 30, stiffness: 400 }}
-            rotationInterval={2000}
-          />
+          <div className="col-span-6">
+            <RotatingText
+              texts={["Student", "Junior Front End"]}
+              mainClassName="px-2 sm:px-2 md:px-3 bg-cyan-300 text-black overflow-hidden py-0.5 sm:py-1 md:py-2 justify-center rounded-lg"
+              staggerFrom={"last"}
+              initial={{ y: "100%" }}
+              animate={{ y: 0 }}
+              exit={{ y: "-120%" }}
+              staggerDuration={0.025}
+              splitLevelClassName="overflow-hidden pb-0.5 sm:pb-1 md:pb-1"
+              transition={{ type: "spring", damping: 30, stiffness: 400 }}
+              rotationInterval={2000}
+            />
+          </div>
           <div className="col-span-6">
             <Lanyard position={[0, 0, 15]} gravity={[0, -40, 0]} />
           </div>
