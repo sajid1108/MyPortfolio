adde32058c6a0633abac210aa3acc65ecb7cf38d
sajid1108 <cahsekolahbelajar@gmail.com>
2025-03-09 23:29:44 +0700

    add blur text

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import Lanyard from "./components/Lanyard/Lanyard";
 import RotatingText from "./components/RotatingText/RotatingText";
 import SplitText from "./components/SplitText/SplitText";
+import BlurText from "./components/BlurText/BlurText";
 
 export default function Home() {
   return (
@@ -9,47 +10,58 @@ export default function Home() {
       <div className="container border mx-auto h-screen">
         <div className="grid grid-cols-12">
           <div className="col-span-6">
-            <div className="flex items-center gap-2">
-              <h1 className="text-2x1 text-white font-bold">I'm a</h1>
-              <RotatingText
-                texts={["Student", "Junior Front End"]}
-                mainClassName="px-2 sm:px-2 md:px-3 bg-[#C6F10E] text-black overflow-hidden py-0.5 sm:py-1 justify-center rounded-lg text-2x1 font-bold inline-flex transition-all"
-                staggerFrom={"last"}
-                initial={{ y: "100%" }}
-                animate={{ y: 0 }}
-                exit={{ y: "-120%" }}
-                staggerDuration={0.025}
-                splitLevelClassName="overflow-hidden pb-0.5 sm:pb-1 md:pb-1"
-                transition={{ type: "spring", damping: 30, stiffness: 400 }}
-                rotationInterval={2000}
-              />
-            </div>
+            <div className="flex flex-col gap-4">
+              <div className="flex items-center gap-2">
+                <h1 className="text-2x1 text-white font-bold">I'm a</h1>
+                <RotatingText
+                  texts={["Student", "Junior Front End"]}
+                  mainClassName="px-2 sm:px-2 md:px-3 bg-[#C6F10E] text-black overflow-hidden py-0.5 sm:py-1 justify-center rounded-lg text-2x1 font-bold inline-flex transition-all"
+                  staggerFrom={"last"}
+                  initial={{ y: "100%" }}
+                  animate={{ y: 0 }}
+                  exit={{ y: "-120%" }}
+                  staggerDuration={0.025}
+                  splitLevelClassName="overflow-hidden pb-0.5 sm:pb-1 md:pb-1"
+                  transition={{ type: "spring", damping: 30, stiffness: 400 }}
+                  rotationInterval={2000}
+                />
+              </div>
 
-            <div className="flex flex-col items-start">
-              <SplitText
-                text="Sajid Abdul Fatah"
-                className="text-2xl font-semibold text-center"
-                delay={50}
-                animationFrom={{
-                  opacity: 0,
-                  transform: "translate3d(0,50px,0)",
-                }}
-                animationTo={{ opacity: 1, transform: "translate3d(0,0,0)" }}
-                threshold={0.2}
-                rootMargin="-50px"
-              />
-              <SplitText
-                text="Front End Developer"
-                className="text-2xl font-semibold text-center text-[#C6F10E]"
-                delay={75}
-                animationFrom={{
-                  opacity: 0,
-                  transform: "translate3d(0,50px,0)",
-                }}
-                animationTo={{ opacity: 1, transform: "translate3d(0,0,0)" }}
-                threshold={0.2}
-                rootMargin="-50px"
-              />
+              <div className="flex flex-col items-start">
+                <SplitText
+                  text="Sajid Abdul Fatah"
+                  className="text-2xl font-semibold text-center"
+                  delay={50}
+                  animationFrom={{
+                    opacity: 0,
+                    transform: "translate3d(0,50px,0)",
+                  }}
+                  animationTo={{ opacity: 1, transform: "translate3d(0,0,0)" }}
+                  threshold={0.2}
+                  rootMargin="-50px"
+                />
+                <SplitText
+                  text="Front End Developer"
+                  className="text-2xl font-semibold text-center text-[#C6F10E]"
+                  delay={75}
+                  animationFrom={{
+                    opacity: 0,
+                    transform: "translate3d(0,50px,0)",
+                  }}
+                  animationTo={{ opacity: 1, transform: "translate3d(0,0,0)" }}
+                  threshold={0.2}
+                  rootMargin="-50px"
+                />
+              </div>
+              <div>
+                <BlurText
+                  text="smkdsnirenvieurdvniduefusvnreind"
+                  delay={150}
+                  animateBy="words"
+                  direction="top"
+                  className="text-xl mb-8"
+                />
+              </div>
             </div>
           </div>
           <div className="col-span-6">
