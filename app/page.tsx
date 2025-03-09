c3d9ee026fe25b7b123d6683f9554b0f27439f5e
sajid1108 <cahsekolahbelajar@gmail.com>
2025-03-09 23:16:02 +0700

    split text delay

--- app/page.tsx
+++ app/page.tsx
@@ -25,11 +25,23 @@ export default function Home() {
               />
             </div>
 
-            <div>
+            <div className="flex flex-col items-start">
               <SplitText
-                text="Hello, My Friends!"
+                text="Sajid Abdul Fatah"
                 className="text-2xl font-semibold text-center"
-                delay={150}
+                delay={50}
+                animationFrom={{
+                  opacity: 0,
+                  transform: "translate3d(0,50px,0)",
+                }}
+                animationTo={{ opacity: 1, transform: "translate3d(0,0,0)" }}
+                threshold={0.2}
+                rootMargin="-50px"
+              />
+              <SplitText
+                text="Front End Developer"
+                className="text-2xl font-semibold text-center text-[#C6F10E]"
+                delay={75}
                 animationFrom={{
                   opacity: 0,
                   transform: "translate3d(0,50px,0)",
