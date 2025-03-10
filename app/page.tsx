28f77743aa9bb462979d7b5bcba197d95dae2fd5
sajid1108 <cahsekolahbelajar@gmail.com>
2025-03-10 22:58:47 +0700

    add animation for rotating text

--- app/page.tsx
+++ app/page.tsx
@@ -3,60 +3,82 @@ import Lanyard from "./components/Lanyard/Lanyard";
 import RotatingText from "./components/RotatingText/RotatingText";
 import SplitText from "./components/SplitText/SplitText";
 import BlurText from "./components/BlurText/BlurText";
+import AnimatedContent from "./components/AnimatedContent/AnimatedContent";
 
 export default function Home() {
   return (
     <div className="min-h-screen overflow-x-hidden bg-[#19222D]">
-      <div className="container border mx-auto h-screen">
+      <div className="container mx-auto h-screen">
         <div className="grid grid-cols-12">
           <div className="col-span-6">
-            <div className="flex flex-col gap-4">
-              <div className="flex items-center gap-2">
-                <h1 className="text-4xl text-white font-bold">I'm a</h1>
-                <RotatingText
-                  texts={["Student", "Junior Front End"]}
-                  mainClassName="px-2 sm:px-2 md:px-3 bg-[#C6F10E] text-black overflow-hidden py-0.5 sm:py-1 justify-center rounded-lg text-4xl font-bold inline-flex transition-all"
-                  staggerFrom={"last"}
-                  initial={{ y: "100%" }}
-                  animate={{ y: 0 }}
-                  exit={{ y: "-120%" }}
-                  staggerDuration={0.025}
-                  splitLevelClassName="overflow-hidden pb-0.5 sm:pb-1 md:pb-1"
-                  transition={{ type: "spring", damping: 30, stiffness: 400 }}
-                  rotationInterval={2000}
-                />
-              </div>
-
-              <div className="flex flex-col items-start">
-                <SplitText
-                  text="Sajid Abdul Fatah"
-                  className="text-6xl font-semibold text-center"
-                  delay={50}
-                  animationFrom={{
-                    opacity: 0,
-                    transform: "translate3d(0,50px,0)",
-                  }}
-                  animationTo={{ opacity: 1, transform: "translate3d(0,0,0)" }}
+            <div className="flex items-center h-full">
+              <div className="flex flex-col gap-6">
+                <AnimatedContent
+                  // className="flex items-center gap-2"
+                  distance={150}
+                  direction="horizontal"
+                  reverse={false}
+                  config={{ tension: 80, friction: 20 }}
+                  initialOpacity={0.2}
+                  animateOpacity
+                  scale={1.1}
                   threshold={0.2}
-                  rootMargin="-50px"
-                />
-                <SplitText
-                  text="Front End Developer"
-                  className="text-6xl font-semibold text-center text-[#C6F10E]"
-                  delay={75}
-                  animationFrom={{
-                    opacity: 0,
-                    transform: "translate3d(0,50px,0)",
-                  }}
-                  animationTo={{ opacity: 1, transform: "translate3d(0,0,0)" }}
-                  threshold={0.2}
-                  rootMargin="-50px"
-                />
-              </div>
-              <div>
+                >
+                  <div className="flex items-center gap-2">
+                    <h1 className="text-4xl text-white font-bold">I'm a</h1>
+                    <RotatingText
+                      texts={["Student", "Junior Front End"]}
+                      mainClassName="px-2 sm:px-2 md:px-3 bg-[#C6F10E] text-black overflow-hidden py-0.5 sm:py-1 justify-center rounded-lg text-4xl font-bold inline-flex transition-all"
+                      staggerFrom={"last"}
+                      initial={{ y: "100%" }}
+                      animate={{ y: 0 }}
+                      exit={{ y: "-120%" }}
+                      staggerDuration={0.025}
+                      splitLevelClassName="overflow-hidden pb-0.5 sm:pb-1 md:pb-1"
+                      transition={{
+                        type: "spring",
+                        damping: 30,
+                        stiffness: 400,
+                      }}
+                      rotationInterval={2000}
+                    />
+                  </div>
+                </AnimatedContent>
+                <div className="flex flex-col items-start">
+                  <SplitText
+                    text="Sajid Abdul Fatah"
+                    className="text-6xl font-semibold text-center"
+                    delay={50}
+                    animationFrom={{
+                      opacity: 0,
+                      transform: "translate3d(0,50px,0)",
+                    }}
+                    animationTo={{
+                      opacity: 1,
+                      transform: "translate3d(0,0,0)",
+                    }}
+                    threshold={0.2}
+                    rootMargin="-50px"
+                  />
+                  <SplitText
+                    text="Front End Developer"
+                    className="text-6xl font-semibold text-center text-[#C6F10E]"
+                    delay={75}
+                    animationFrom={{
+                      opacity: 0,
+                      transform: "translate3d(0,50px,0)",
+                    }}
+                    animationTo={{
+                      opacity: 1,
+                      transform: "translate3d(0,0,0)",
+                    }}
+                    threshold={0.2}
+                    rootMargin="-50px"
+                  />
+                </div>
                 <BlurText
                   text="smkdsnirenvieurdvniduefusvnreind"
-                  delay={150}
+                  delay={75}
                   animateBy="words"
                   direction="top"
                   className="text-xl mb-8"
